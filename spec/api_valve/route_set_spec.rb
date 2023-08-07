f58d14b445e642fdcebb5915c689227d7f110eff
Konstantin Munteanu <konstantin.munteanu@kaiahealth.com>
2023-08-07 10:04:27 +0200

    rubocop

--- spec/api_valve/route_set_spec.rb
+++ spec/api_valve/route_set_spec.rb
@@ -2,7 +2,7 @@ RSpec.describe ApiValve::RouteSet do
   subject(:route_set) { described_class.new }
 
   let(:rack_response) { [200, {'Content-Type' => 'text/plain'}, ['OK']] }
-  let(:route_proc) { proc { rack_response } } # rubocop:disable RSpec/VerifiedDoubles
+  let(:route_proc) { proc { rack_response } }
 
   before do
     allow(route_proc).to receive(:call).and_call_original
@@ -47,9 +47,9 @@ RSpec.describe ApiValve::RouteSet do
         end
 
         it 'raises ApiValve::Error::NotFound if no patch matches' do
-          expect {
+          expect do
             subject.match(env.merge('PATH_INFO' => '/notexists'))
-          }.to raise_error(ApiValve::Error::NotRouted)
+          end.to raise_error(ApiValve::Error::NotRouted)
         end
       end
     end
@@ -57,10 +57,10 @@ RSpec.describe ApiValve::RouteSet do
 
   describe '#unshift' do
     let(:define_routes) do
-      route_set.get(%r{^/start}, {}, &proc1)
+      route_set.get(%r{^/start}, {}, &proc_one)
     end
-    let(:proc1) { proc { rack_response } }
-    let(:proc2) { proc { rack_response } }
+    let(:proc_one) { proc { rack_response } }
+    let(:proc_two) { proc { rack_response } }
     let(:env) do
       {
         'PATH_INFO'      => '/start',
@@ -69,15 +69,15 @@ RSpec.describe ApiValve::RouteSet do
     end
 
     before do
-      [proc1, proc2].each { |pr| allow(pr).to receive(:call).and_call_original }
+      [proc_one, proc_two].each { |pr| allow(pr).to receive(:call).and_call_original }
     end
 
     it 'adds the route the the front' do
-      route_set.unshift(:get, %r{^/start}, {}, &proc2)
+      route_set.unshift(:get, %r{^/start}, {}, &proc_two)
       route, _match_data = route_set.match(env)
       route.call
-      expect(proc1).not_to have_received(:call)
-      expect(proc2).to have_received(:call)
+      expect(proc_one).not_to have_received(:call)
+      expect(proc_two).to have_received(:call)
     end
   end
 end
